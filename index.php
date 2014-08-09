e50a256f0fe002f2dbda7ad03835bced8e3c7d5a
Luke Brown <me@luke.sx>
2014-08-09 19:19:10 +0100

    Updated CSS for second navbar and index page

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 		<?php include 'assets/navigation.php' ?>
 		<div id="slider">
 			<div class="ticker">
-				<h2>Server management finally done differently</h2>
-				<h2>We treat you as a name and not a number</h2>
+				<h2>Server management finally done right</h2>
+				<h2>A complete new take to hosting your project</h2>
 				<h2>The best Minecraft hosting in the galaxy</h2>
 				<h2>Take your server project to the next level</h2>
 			</div>
@@ -46,32 +46,80 @@
 			<h1>What are you?</h1>
 				<?php include 'assets/servicebtns.php' ?>
 			<h1>Why Choose us?</h1>
+
 			<div class="row">
-				<div class="col-md-6">
-					<h3>Follow Us</h3>
-					<a class="twitter-timeline" href="https://twitter.com/GS_Hosting" data-widget-id="416324409570238464">Tweets by @GS_Hosting</a>
-					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-				</div>
-				<div class="col-md-6">
-					<h3>Our Mission</h3>
-					<div class="mission">
-						<p>We are a dedicated hosting company directed and motivated to providing our clients with the <span>best possible services</span> without restrictions for the <span>lowest price possible</span>. We don't believe in hidden costs or charge premium for services. Our mission goal is to provide the world with <span>affordable hosting</span>.</p>
-						<a href="https://twitter.com/GS_Hosting">
-							<span class="fa-stack fa-lg">
-								<i class="fa fa-circle fa-stack-2x"></i>
-								<i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-							</span>
-							@GS_Hosting
-						</a>
-					</div>
-					<div class="clearfix"></div>
-					<h3>About Us</h3>
-					<div class="mission">
-						<p>Galactic Servers is a fully registered Ltd firm which was set up to break into the hosting market for affordable hosting with quality services. With a growing group of dedicated staff our team is determined to help bring the best support available with affordable pricing to everyone.</p>
-						<p>Whilst setting up and continuing to explain, Galactic Servers will continue to provide a quality services to its loyal member with the community in mind. Providing hosting solutions is our main service but we also accompanying for many other services which you can find through our site.</p>
+				<div class="col-md-10 col-md-offset-1">
+					<div class="row">
+						
+						<div class="col-md-6">
+
+							
+							<div class="mission featbox">
+								<i class="fa fa-lock"></i>
+								<h3>Security &amp; Protection</h3>
+								<p>Nearly all of our servers come with <span>guaranteed DDoS protection</span> to protect your clients and your data from disruption of service. We also employ only the <spam>latest security methods</span> to keep your <span>data secure</span> and out of reach from competitors and hackers.</p>
+							</div>
+
+						</div>
+
+						<div class="col-md-6">
+
+							<div class="mission featbox">
+								<i class="fa fa-signal"></i>
+								<h3>Performance &amp; Quality</h3>
+								<p>We only deploy the <span>latest and greatest</span> from the vast range of server at our disposal and we regularly check the <span>performance</span> levels of all of our client's servers to ensure the expected <span>levels of service</span> do not drop below the standards we aim to achieve.</p>
+							</div>
+
+						</div>
+						
+						<div class="col-md-6">
+
+							
+							<div class="mission featbox">
+								<i class="fa fa-user"></i>
+								<h3>Personal Support</h3>
+								<p>Every client gets their very own <span>dedicated staff member</span> assigned to their account to guide them through thick and thin at Galactic Servers. Your assigned account manager will have an <span>array of skills</span> and can be adapted to your project at any stage.</p>
+							</div>
+
+						</div>
+
+						<div class="col-md-6">
+
+							<div class="mission featbox">
+								<i class="fa fa-terminal"></i>
+								<h3>Other Opportunities</h3>
+								<p>While you remain a client at <span>Galactic Servers</span> we will do as much as we possible can to <span>offer you the world</span>. Or more realistically, our in house developers are always available for hire under discounted prices for your <span>web</span>, <span>server</span> and <span>coding</span> needs.</p>
+								<a href="https://twitter.com/GS_Hosting">
+								</a>
+							</div>
+
+						</div>
+
+						<div class="col-md-6">
+
+							<div class="mission featbox">
+								<i class="fa fa-hdd-o"></i>
+								<h3>Data Reconnaissance</h3>
+								<p>We know how extremely frustrating losing your data can be. That's why at every level, we do our very best to ensure that <span>your data</span> cannot be <span>lost</span>, <span>stolen</span> or <span>corrupted</span>. But in the unlikely case that we cannot stop the inevitable we also employ monthly backups to <span>external servers</span> of all your files for peace of mind.
+								</p>
+							</div>
+						</div>
+
+						<div class="col-md-6">
+
+							<div class="mission featbox">
+								<i class="fa fa-gears"></i>
+								<h3>Constant Adaptability</h3>
+								<p>At your hopefully long time at <span>Galactic Servers</span> we will do our best to adapt to your needs. <span>Constantly</span> improving on your feedback and suggestions allows us to become the <span>best business solution</span> you will ever need. Running multiple projects with us has its perks, by combining different project you can <span>combine fees</span> and <span>developement times</span>.</p>
+							
+							</div>
+						</div>						
 					</div>
 				</div>
 			</div>
+
+			<h1>Let's get started!</h1>
+
 		</div>
 	</div>
 	<?php include 'assets/footer.php' ?>
